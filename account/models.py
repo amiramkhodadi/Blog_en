e647a1f41849e7c8357bfc4eb609232a3b16b722
Amir khodadi <amiramkhodadi@gmail.com>
2025-05-20 13:55:45 +0330

    add profile for user with image

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     father_name = models.CharField(max_length=50)
     meli_code = models.CharField(max_length=10)
-    image = models.ImageField(upload_to='profile/profile_pics')
+    image = models.ImageField(upload_to='profile/profile_pics',null=True ,blank=True)
 
 
     def __str__(self):
