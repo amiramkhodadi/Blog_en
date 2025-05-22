87c7ba7792045f6d6ad1f048aae40deafeeea20e
Amir khodadi <amiramkhodadi@gmail.com>
2025-05-22 23:12:30 +0330

    add recent article to home page

--- home/views.py
+++ home/views.py
@@ -7,4 +7,4 @@ from blog.models import Article
 def home(request):
     articles = Article.objects.all()
     recent_articles = Article.objects.all()[:3]
-    return render(request, 'home/index.html',{'articles':articles})
+    return render(request, 'home/index.html',{'articles':articles , 'recent_articles':recent_articles})
