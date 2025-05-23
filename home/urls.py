2f945097d8281fcf181a23e54c17d8fb639b8f61
Amir khodadi <amiramkhodadi@gmail.com>
2025-05-23 22:51:37 +0330

    nothing

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,5 @@ from home import views
 urlpatterns = [
     path('', views.home, name='home'),
 # path('', views.home, name='home'),
-# path('', views.home, name='home'),
-# path('', views.home, name='home'),
+
 ]
