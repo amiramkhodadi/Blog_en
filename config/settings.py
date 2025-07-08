5b79c74341588b75921d2d3c2b259dd236e93853
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-08 11:01:14 +0330

    پیام جدید دلخواه

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_en',
+        'USER': 'root',
+        'PASSWORD': 'Amir1380',
+        'HOST': 'localhost',  # یا '127.0.0.1'
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
