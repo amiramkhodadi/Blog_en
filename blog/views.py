159f3d77790d8f4a572b6e8013014bf0acc829b0
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-08 15:08:48 +0330

    add pagination for all articles page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from blog.models import Article, Category
 
@@ -9,7 +11,15 @@ def article_detail(request , slug):
 
 def article_list(request ):
     articles = Article.objects.all()
-    return render(request , 'blog/article_list.html' , {'articles' : articles })
+    paginator = Paginator(articles, 1)
+    page = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request , 'blog/article_list.html' , {'articles' : page_obj })
 
 
 def category_detail(request , pk=None):
