f4f8fbc7178bbc5de2e0122401f7d5c7f3a34190
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-09 12:29:07 +0330

    shart gozari braye sakhte cm jadid

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from account import views
 from django.urls import path
-app_name = 'account'
+# app_name = 'account'
 urlpatterns = [
     path('login/',views.user_login,name='login'),
     path('logout/',views.user_logout,name='logout'),
