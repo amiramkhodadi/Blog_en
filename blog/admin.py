7e61be6e3149d65a0c7ad049b9cb91e77145f086
Amir khodadi <amiramkhodadi@gmail.com>
2025-07-17 10:30:06 +0330

    create custom filter in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,15 +3,42 @@ from django.utils.autoreload import start_django
 
 from blog.models import Article, Category, Comment, Message
 
+
+class FilterByTitle(admin.SimpleListFilter):
+    title = "واژه های پر کابرد"
+    parameter_name = "title"
+
+    def lookups(self, request, model_admin):
+
+        return [
+            ("python", "پایتون"),
+            ("html", "اچ تی ام ال"),
+            ("css", "سی اس اس"),
+
+        ]
+
+    def queryset(self, request, queryset):
+
+        if self.value() :
+            return queryset.filter(
+                title__icontains=self.value()
+            )
+
+
+
+
+
 # Register your models here.
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'status' , 'author']
+    list_display = ['title','content' , 'status' , 'author']
 
              # in feture kheili khobe v miad to on safe y filter sazio ro bar asas model ha bramon anjam mide
-    list_filter = ['status']
+    list_filter = ['status',FilterByTitle]
              # ba estefade azin   dar lhze roye list bedon vared shdn b onject mitonim taiqrat ro emal konim
     list_editable = ('author',)
+            # in miad y search input dar balaye safe brae ma mizare k khodesh bar asas in feild haye zir toshon search mikone
+    search_fields = ['title' , 'content']
 
 
 
